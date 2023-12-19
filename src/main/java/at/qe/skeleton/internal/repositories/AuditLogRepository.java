c9b8854955ded615dba5207349f5ffbe004fefa8
csaw6568 <simon.c.froehlich@student.uibk.ac.at>
2023-12-19 11:57:23 +0100

    added a method to search the audit log repository logs for a specific username

--- src/main/java/at/qe/skeleton/internal/repositories/AuditLogRepository.java
+++ src/main/java/at/qe/skeleton/internal/repositories/AuditLogRepository.java
@@ -1,6 +1,13 @@
 package at.qe.skeleton.repositories;
 
 import at.qe.skeleton.model.AuditLog;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AuditLogRepository extends AbstractRepository<AuditLog, Long> {
+
+    @Query("SELECT al FROM AuditLog al WHERE al.message LIKE %:username% AND al.message LIKE '%has changed to the role(s)%'")
+    List<AuditLog> findLogsForUser(@Param("username") String username);
 }
