a06ae3899e40fea93f2a4bfd540f9b0799f15519
Leonardo Pedri <leonardo.pedri@uibk.ac.at>
2023-12-27 12:26:48 +0100

    refactor weather api service

--- src/main/java/at/qe/skeleton/internal/services/CurrentAndForecastAnswerService.java
+++ src/main/java/at/qe/skeleton/internal/services/CurrentAndForecastAnswerService.java
@@ -3,37 +3,44 @@ package at.qe.skeleton.internal.services;
 import at.qe.skeleton.external.model.currentandforecast.CurrentAndForecastAnswerDTO;
 import at.qe.skeleton.internal.model.CurrentAndForecastAnswer;
 import at.qe.skeleton.internal.repositories.CurrentAndForecastAnswerRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-/** Service for accessing and manipulating CurrentAndForecastWeather data. */
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Service for accessing and manipulating CurrentAndForecastWeather data.
+ */
 @Component
 @Scope("application")
 public class CurrentAndForecastAnswerService {
 
-  @Autowired private CurrentAndForecastAnswerRepository currentAndForecastAnswerRepository;
+    @Autowired
+    private CurrentAndForecastAnswerRepository currentAndForecastAnswerRepository;
 
-  /**
-   * Takes the API call that has been serialized to a Json string and serializes it to a byte array.
-   * This is then set as a field of a CurrentAndForecastWeather (the entity) object and persisted
-   * via the repository.
-   *
-   * @param currentAndForecastAnswerString the API call that has been serialized to a Json string
-   * @return the saved CurrentAndForecastWeather object
-   */
-  public CurrentAndForecastAnswer saveWeather(String currentAndForecastAnswerString) {
-    CurrentAndForecastAnswer currentAndForecastAnswer = new CurrentAndForecastAnswer();
-    currentAndForecastAnswer.setWeatherData(
-        currentAndForecastAnswerString.getBytes(StandardCharsets.UTF_8));
-    return currentAndForecastAnswerRepository.save(currentAndForecastAnswer);
-  }
+    /**
+     * Takes the API call that has been serialized to a Json string and serializes it to a byte array.
+     * This is then set as a field of a CurrentAndForecastWeather (the entity) object and persisted
+     * via the repository.
+     *
+     * @param answerDTO the API call that has been mapped to a CurrentAndForecastAnswerDTO
+     * @return the saved CurrentAndForecastWeather object
+     */
+    public CurrentAndForecastAnswer saveWeather(@NotNull CurrentAndForecastAnswerDTO answerDTO) throws FailedToSerializeDTOException {
+        CurrentAndForecastAnswer currentAndForecastAnswer = new CurrentAndForecastAnswer();
+        currentAndForecastAnswer.setWeatherData(serializeDTO(answerDTO));
+        return currentAndForecastAnswerRepository.save(currentAndForecastAnswer);
+    }
 
   /**
    * Retrieve the persisted CurrentAndForecastAnswer objects by their id.
@@ -68,29 +75,53 @@ public class CurrentAndForecastAnswerService {
       return Collections.emptyList();
     }
   }
+    /**
+     * Retrieve all the persisted CurrentAndForecastAnswer objects.
+     *
+     * @return a collection of all the currently stored CurrentAndForecastAnswer API calls as DTOs
+     */
+    public Collection<CurrentAndForecastAnswerDTO> getAllCurrentAndForecastWeather() throws FailedJsonToDtoMappingException {
+        Collection<CurrentAndForecastAnswer> allWeatherData = currentAndForecastAnswerRepository.findAll();
+        Collection<CurrentAndForecastAnswerDTO> allWeatherDataDTOs = new ArrayList<>();
+        for (CurrentAndForecastAnswer currentAndForecastAnswer : allWeatherData) {
+            allWeatherDataDTOs.add(deserializeDTO(currentAndForecastAnswer.getWeatherData()));
+        }
+        return allWeatherDataDTOs;
+    }
 
-  /**
-   * Deserialize a mapped CurrentAndForecastAnswer into a CurrentAndForecastAnswerDTO
-   *
-   * @param currentAndForecastAnswer the persisted CurrentAndForecastAnswer to deserialize
-   * @return the deserialized CurrentAndForecastAnswer mapped to a CurrentAndForecastAnswerDTO
-   * @throws FailedJsonToDtoMappingException when the mapping fails
-   */
-  public CurrentAndForecastAnswerDTO mapSerializedWeatherToDto(
-      CurrentAndForecastAnswer currentAndForecastAnswer) throws FailedJsonToDtoMappingException {
-    ObjectMapper mapper = new ObjectMapper().findAndRegisterModules();
-    try {
-      return mapper.readValue(
-          currentAndForecastAnswer.getWeatherData(), CurrentAndForecastAnswerDTO.class);
-    } catch (IOException e) {
-      throw new FailedJsonToDtoMappingException(
-          "Mapping of the retrieved CurrentAndForecastWeather Json back to DTO failed");
+
+    /**
+     * Deserialize a mapped CurrentAndForecastAnswer into a CurrentAndForecastAnswerDTO
+     *
+     * @param serializedDTO the serialized CurrentAndForecastAnswerDTO to deserialize
+     * @return the deserialized CurrentAndForecastAnswerDTO
+     * @throws FailedJsonToDtoMappingException when the mapping fails
+     */
+    public CurrentAndForecastAnswerDTO deserializeDTO(
+            @NotNull byte[] serializedDTO) throws FailedJsonToDtoMappingException {
+        ObjectMapper mapper = new ObjectMapper().findAndRegisterModules();
+        try {
+            return mapper.readValue(
+                    serializedDTO, CurrentAndForecastAnswerDTO.class);
+        } catch (IOException e) {
+            throw new FailedJsonToDtoMappingException();
+        }
     }
-    // When there is nothing in the db, .getWeatherData() returns null and thus throws a
-    // NullPointerException.
-    // If it isn't caught here, the program will stop
-    catch (NullPointerException e) {
-      return null;
+
+    /**
+     * Serialize api answers to be saved in the database as blobs
+     *
+     * @param answerDTO the api call answer
+     * @return the serialized api call
+     * @throws FailedToSerializeDTOException when the serialization fails
+     */
+    public byte[] serializeDTO(@NotNull CurrentAndForecastAnswerDTO answerDTO) throws FailedToSerializeDTOException {
+        ObjectMapper mapper =
+                new ObjectMapper().findAndRegisterModules().enable(SerializationFeature.INDENT_OUTPUT);
+        try {
+            return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(answerDTO).getBytes(StandardCharsets.UTF_8);
+        } catch (JsonProcessingException e) {
+            throw new FailedToSerializeDTOException();
+        }
     }
-  }
 }
---
+++ src/main/java/at/qe/skeleton/internal/services/FailedToSerializeDTOException.java
@@ -0,0 +1,7 @@
+package at.qe.skeleton.internal.services;
+
+public class FailedToSerializeDTOException extends Exception {
+    public FailedToSerializeDTOException() {
+        super();
+    }
+}
