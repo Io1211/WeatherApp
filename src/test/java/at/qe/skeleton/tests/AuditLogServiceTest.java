b6bc137bcadd0f7cb09e11b617fd6f78c0570eff
csaw6568 <simon.c.froehlich@student.uibk.ac.at>
2024-01-07 21:12:51 +0100

    updated test cases for AuditServiceLogTest

--- src/test/java/at/qe/skeleton/tests/AuditLogServiceTest.java
+++ src/test/java/at/qe/skeleton/tests/AuditLogServiceTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import at.qe.skeleton.internal.model.Userx;
+import at.qe.skeleton.internal.model.UserxRole;
 import at.qe.skeleton.internal.services.AuditLogService;
 import at.qe.skeleton.internal.model.AuditLog;
 
@@ -26,32 +27,34 @@ public class AuditLogServiceTest {
     @Test
     void saveEntryTest() {
         String message = "Test";
-
+        doNothing().when(auditLogRepository).save(any(AuditLog.class));
         auditLogService.saveEntry(message);
-
-        // TO-DO
-
+        verify(auditLogRepository, times(1)).save(argThat(argument -> {
+            return argument.getMessage().equals(message);
+        }));
     }
 
     @Test
     void saveDeletedUserEntryTest() {
         Userx userx = new Userx();
         userx.setUsername("testUser");
-        userx.setRoles(Sets.newSet(UserxRole.ROLE_USER));
-
+        userx.setRoles(Sets.newSet(UserxRole.PREMIUM));
         auditLogService.saveDeletedUserEntry(userx);
-
-        // TO-DO
+        doNothing().when(auditLogService).saveEntry(anyString());
+        auditLogService.saveDeletedUserEntry(userx);
+        // check for correct delete message
+        verify(auditLogService, times(1)).saveEntry("User testUser with role(s) PREMIUM has been deleted.");
     }
+
 
     @Test
     void saveCreatedUserEntryTest() {
         Userx userx = new Userx();
         userx.setUsername("testUser");
-        userx.setRoles(Sets.newSet(UserxRole.ROLE_USER));
-
+        userx.setRoles(Sets.newSet(UserxRole.ADMIN));
+        doNothing().when(auditLogService).saveEntry(anyString());
         auditLogService.saveCreatedUserEntry(userx);
-
-        // TO-DO
+        // check for correct delete message
+        verify(auditLogService, times(1)).saveEntry("User testUser with role(s) ADMIN has been saved.");
     }
 }
