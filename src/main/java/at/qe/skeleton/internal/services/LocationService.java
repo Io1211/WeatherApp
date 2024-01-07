8b53eb112834bd6ff1b95fde8ba29a4f0c452ac0
christopherstief <christopher.stief@student.uibk.ac.at>
2024-01-07 20:55:28 +0100

    added Documentation for handleLocationSearch in LocationService.java.

--- src/main/java/at/qe/skeleton/internal/services/LocationService.java
+++ src/main/java/at/qe/skeleton/internal/services/LocationService.java
@@ -14,6 +14,7 @@ import java.time.ZonedDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
 
 @Component
 @Scope("application")
@@ -31,6 +32,18 @@ public class LocationService {
     return geocodingApiRequestService.retrieveLocationLonLat(locationName);
   }
 
+  /**
+   * Searches for current Data with the given Location Name. If there is no db entry for the
+   * Location Object with the corresponding Lon/Lat Id, a new one is being created. If there is an
+   * existing Location in the db and it is not older than one hour, this Location will be returned.
+   * If there is an older Location in the db, the Weather Api will be called and the Location in the
+   * db will get updated with the new Weather and this Location will be returned. <br>
+   * <br>
+   *
+   * @param locationSearchString the name of the location
+   * @return a location with weatherdata not older than the last full hour.
+   * @throws FailedToSerializeDTOException if there occur problems with the DTO serialization.
+   */
   public Location handleLocationSearch(String locationSearchString)
       throws FailedToSerializeDTOException {
     // This method covers 3 cases:
@@ -40,13 +53,15 @@ public class LocationService {
     Location location = new Location();
     LocationAnswerDTO locationAnswerDTO = callApi(locationSearchString);
     CurrentAndForecastAnswerDTO weatherDTO;
+    // Case 1:
     if (locationAlreadyPersisted(locationAnswerDTO)) {
       location = getLocation(locationAnswerDTO);
       if (locationHasUpToDateWeatherData(location)) {
         return location;
       }
     }
-    // Cases 2. & 3. can be handled the same, as overwriting the id, city, state etc. with the very
+    // Cases 2 & 3:
+    // they can be handled the same, as overwriting the id, city, state etc. with the very
     // same data doesn't make a difference in the data per se and simplifies this method slightly by
     // combining the logic of the two cases.
     location.setId(locationAnswerDTO.latitude(), locationAnswerDTO.longitude());
