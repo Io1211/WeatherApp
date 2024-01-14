eb6c8356b5f30db4714a6a2b2dcab082ff138e03
csaw1115 <andreas.enk@student.uibk.ac.at>
2024-01-15 00:22:09 +0100

    update equals test to account for bidirectional relationship of user and favorite

--- src/test/java/at/qe/skeleton/tests/EqualsImplementationTest.java
+++ src/test/java/at/qe/skeleton/tests/EqualsImplementationTest.java
@@ -1,10 +1,12 @@
 package at.qe.skeleton.tests;
 
+import at.qe.skeleton.internal.model.Favorite;
 import at.qe.skeleton.internal.model.Userx;
 import at.qe.skeleton.internal.model.UserxRole;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.Warning;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
 
 /**
  * Tests to ensure that each entity's implementation of equals conforms to the contract. See
@@ -21,8 +23,13 @@ public class EqualsImplementationTest {
     user1.setUsername("user1");
     Userx user2 = new Userx();
     user2.setUsername("user2");
+    Favorite favorite1 = new Favorite();
+    ReflectionTestUtils.setField(favorite1, "id", 0L);
+    Favorite favorite2 = new Favorite();
+    ReflectionTestUtils.setField(favorite2, "id", 1L);
     EqualsVerifier.forClass(Userx.class)
         .withPrefabValues(Userx.class, user1, user2)
+        .withPrefabValues(Favorite.class, favorite1, favorite2)
         .suppress(Warning.STRICT_INHERITANCE, Warning.ALL_FIELDS_SHOULD_BE_USED)
         .verify();
   }
